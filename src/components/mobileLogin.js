e22b74d54cd27b37a8101d94eec0ced253416dbc
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2023-06-15 20:14:33 +0100

    Work is done here

--- src/components/mobileLogin.js
+++ src/components/mobileLogin.js
@@ -82,85 +82,87 @@ function MobileLogin() {
   };
 
   return (
-    <div className=" w-screen min-[548px]:w-[60%] lg:w-[30%] font-manrope ">
-      <h2 className=" text-black text-center font-bold pt-14 pb-8  text-[22px]">
-        Log in or sign up to continue
-      </h2>
-      <div className=" flex justify-center pb-6">
-        <button className=" flex px-20 w-full border-none rounded-xl justify-center items-center">
-          <img
-            className=""
-            src={google}
-            onClick={onGoogleClick}
-            alt=" google"
-          />
-          <p
-            onClick={onGoogleClick}
-            className=" cursor-pointer mt-1 mx-2 text-[18px] font-extrabold"
-          >
-            Continue with Google
-          </p>
-        </button>
-      </div>
-      <div className="flex justify-center items-center pb-6">
-        <div className=" ml-4 border w-[48%]"></div>
-        <p className=" px-3 text-[#8F8F8F]">or</p>
-        <div className="border mr-4 w-[48%] "></div>
-      </div>
-      <div className=" ml-8 justify-center items-center">
-        <form className="  flex flex-col w-[90%]  pb-6 ">
-          <input
-            type="text"
-            id="email"
-            onChange={onChange}
-            name="email"
-            value={email}
-            autoComplete="On"
-            placeholder="Enter your email address"
-            className="text-[#8F8F8F] 	mb-6 pb-6 bg-[#EDEDED] block w-full px-6 py-5 rounded-xl focus:outline-none focus:border-sky-500 placeholder:text-[#8F8F8F] placeholder-gray-500::placeholder text-lg font-semibold"
-          />
-          <input
-            type="password"
-            id="password"
-            onChange={onChange}
-            name="password"
-            value={password}
-            autoComplete="On"
-            placeholder="Password"
-            className="text-[#8F8F8F] placeholder-gray-500::placeholder text-lg font-semibold bg-[#EDEDED] block w-full px-6 py-5 rounded-xl focus:outline-none focus:border-sky-500 placeholder:text-[#8F8F8F]"
-          />
-          <button
-            onClick={onSubmit}
-            className="text-white bg-[#0052F6] w-full px-4 rounded-xl h-[60px] font-bold text-[16px] mt-5 "
-          >
-            Continue with email
+    <div className=" w-screen min-[420px]:w-[60%] lg:w-[30%] font-manrope mx-auto flex justify-center items-center ">
+      <div className="justify-center w-[420px]">
+        <h2 className=" text-black text-center font-bold pt-14 pb-8  text-[22px]">
+          Log in or sign up to continue
+        </h2>
+        <div className=" flex justify-center pb-6">
+          <button className=" flex px-20 w-full border-none rounded-xl justify-center items-center">
+            <img
+              className=""
+              src={google}
+              onClick={onGoogleClick}
+              alt=" google"
+            />
+            <p
+              onClick={onGoogleClick}
+              className=" cursor-pointer mt-1 mx-2 text-[18px] font-extrabold"
+            >
+              Continue with Google
+            </p>
           </button>
-        </form>
-        <div className="flex justify-center mt-5 w-100%">
-          <p className=" text-[#8f8f8f] text-center w-85 ">
-            By continuing, you acknowledge that you have read and understand,
-            and agree to Teeket's{" "}
-            <span
-              style={{
-                fontWeight: "700",
-                textDecoration: "underline",
-                color: "#101010",
-              }}
+        </div>
+        <div className="flex justify-center items-center pb-6">
+          <div className=" ml-4 border w-[48%]"></div>
+          <p className=" px-3 text-[#8F8F8F]">or</p>
+          <div className="border mr-4 w-[48%] "></div>
+        </div>
+        <div className=" ml-8 justify-center items-center">
+          <form className="  flex flex-col w-[90%]  pb-6 ">
+            <input
+              type="text"
+              id="email"
+              onChange={onChange}
+              name="email"
+              value={email}
+              autoComplete="On"
+              placeholder="Enter your email address"
+              className="text-[#8F8F8F] 	mb-6 pb-6 bg-[#EDEDED] block w-full px-6 py-5 rounded-xl focus:outline-none focus:border-sky-500 placeholder:text-[#8F8F8F] placeholder-gray-500::placeholder text-lg font-semibold"
+            />
+            <input
+              type="password"
+              id="password"
+              onChange={onChange}
+              name="password"
+              value={password}
+              autoComplete="On"
+              placeholder="Password"
+              className="text-[#8F8F8F] placeholder-gray-500::placeholder text-lg font-semibold bg-[#EDEDED] block w-full px-6 py-5 rounded-xl focus:outline-none focus:border-sky-500 placeholder:text-[#8F8F8F]"
+            />
+            <button
+              onClick={onSubmit}
+              className="text-white bg-[#0052F6] w-full px-4 rounded-xl h-[60px] font-bold text-[16px] mt-5 "
             >
-              Terms of Service
-            </span>{" "}
-            and{" "}
-            <span
-              style={{
-                fontWeight: "700",
-                textDecoration: "underline",
-                color: "#101010",
-              }}
-            >
-              Privacy Policy
-            </span>
-            .
-          </p>
+              Continue with email
+            </button>
+          </form>
+          <div className="flex justify-center mt-5 w-100%">
+            <p className=" text-[#8f8f8f] text-center w-85 ">
+              By continuing, you acknowledge that you have read and understand,
+              and agree to Teeket's{" "}
+              <span
+                style={{
+                  fontWeight: "700",
+                  textDecoration: "underline",
+                  color: "#101010",
+                }}
+              >
+                Terms of Service
+              </span>{" "}
+              and{" "}
+              <span
+                style={{
+                  fontWeight: "700",
+                  textDecoration: "underline",
+                  color: "#101010",
+                }}
+              >
+                Privacy Policy
+              </span>
+              .
+            </p>
+          </div>
         </div>
       </div>
     </div>
