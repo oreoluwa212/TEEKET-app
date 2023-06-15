d0b6a6dfaedace60b8448a0486d5f86d86325bab
Oreoluwa Ajayi <oreoluwaajayyiruth@gmail.com>
2023-06-15 15:45:48 +0100

    Google signUp works fine

--- src/components/mobileLogin.js
+++ src/components/mobileLogin.js
@@ -1,34 +1,80 @@
 import google from "../img/google.png";
-// import Button from "../components/Button"; 
-// import { Link } from "react-router-dom";
+import { dataBase } from "../firebase.config";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { auth } from "../firebase.config";
+
 import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
   //signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
+
+import { doc, setDoc, getDoc, serverTimestamp } from "firebase/firestore";
 
 import { toast } from "react-toastify";
 
 
 function MobileLogin() {
-  const [ email, setEmail ] = useState("")
-  const [ password, setPassword ] = useState("")
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({ email: "", password: "" });
 
-  const signUp = async (e) => {
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    createUserWithEmailAndPassword( auth, email, password )
-    .then((userCredential) => {
-      console.log(userCredential);
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(dataBase, "users", user.uid), formDataCopy);
+      navigate("/landingpage");
       toast.success("Login was successful!");
-
-    }).catch((error) => {
-      console.log(error);
+    } catch (error) {
       toast.error("Something went wrong");
+    }
+  };
 
-    });
-  }
+  const onGoogleClick = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      // check for user
+      const docRef = doc(dataBase, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(doc(dataBase, "users", user.uid), {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/");
+      toast.success("Login was successful!");
+    } catch (error) {
+      toast.error("Could not authorize with Google.");
+    }
+  };
   return (
     <div className="mx-auto w-[90%] min-[400px]:w-[90vw] mt-6 lg:w-[30%] font-manrope ">
       <h2 className=" text-black text-center font-bold py-14  text-[20px]">
@@ -36,8 +82,15 @@ function MobileLogin() {
       </h2>
       <div className=" flex justify-center pb-6">
         <button className=" flex px-20 w-full border-none rounded-xl justify-center items-center">
-          <img className="" src={google} alt=" google" />
-          <p className=" mt-1 mx-2 font-bold">Continue with Google</p>
+          <img
+            className=""
+            src={google}
+            onClick={onGoogleClick}
+            alt=" google"
+          />
+          <p onClick={onGoogleClick} className=" mt-1 mx-2 font-bold">
+            Continue with Google
+          </p>
         </button>
       </div>
       <div className="flex justify-center items-center pb-6">
@@ -47,12 +100,12 @@ function MobileLogin() {
       </div>
       <div className="">
         <form
-          onSubmit={signUp}
           className="grid w-full justify-items-center pb-6"
         >
           <input
-            type="email"
-            onChange={(e) => setEmail(e.target.value.toLowerCase())}
+            type="text"
+            onChange={onChange}
+            name="email"
             value={email}
             autoComplete="On"
             placeholder="Enter your email address"
@@ -60,13 +113,15 @@ function MobileLogin() {
           />
           <input
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={onChange}
+            name="password"
             value={password}
             autoComplete="On"
             placeholder="Password"
             className="text-[#8F8F8F] text-base bg-[#EDEDED] block w-full px-6 py-5 rounded-xl focus:outline-none focus:border-sky-500 placeholder:text-[#8F8F8F]"
           />
-          <button className="text-white bg-[#0052F6] w-full px-4 rounded-xl h-[60px] font-bold text-[16px] mt-5 ">
+          <button 
+          onClick={onSubmit} className="text-white bg-[#0052F6] w-full px-4 rounded-xl h-[60px] font-bold text-[16px] mt-5 ">
             Continue with email
           </button>
         </form>
--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,24 +1,28 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+
+
 import { getAnalytics } from "firebase/analytics";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBFei2oIBX2jqHZ-PZLinedCufebu4BLdw",
   authDomain: "teeket-dde6f.firebaseapp.com",
   projectId: "teeket-dde6f",
   storageBucket: "teeket-dde6f.appspot.com",
   messagingSenderId: "240236719932",
-  appId: "1:240236719932:web:cd9f343feb650a51d23839",
-  measurementId: "G-3PFNXD0RHG",
+  appId: "1:240236719932:web:739830d24ebb207dd23839",
+  measurementId: "G-LR8EWXVQW3"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 console.log(analytics);
-export const auth = getFirestore();
+export const dataBase = getFirestore();
+
+
